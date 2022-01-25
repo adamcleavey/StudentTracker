61c401d980eb696b1cddb1266744f4b06c311521
Adam Levy <adam.k.levy@me.com>
2022-01-24 23:37:16 -0500

    stable

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import useFirebase from "./hooks/useFirebase";
 
 function App() {
 
-  const firebase = useFirebase();
   return (
     <div className="App">
       <header className="App-header">
@@ -13,19 +11,6 @@ function App() {
         <h2>
           Student Tracker
         </h2>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        {firebase && 
-        <>
-          useFirebase hook is working!
-        </>
-      }
       </header>
     </div>
   );
--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,7 +1,5 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const config = {
@@ -15,10 +13,6 @@ const config = {
     measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENTID
 };
 
-let instance = null;
-// Initialize Firebase
-export default function getFirebase() {
-    if (instance) return instance;
-    instance = initializeApp(config);
-    return instance;
-}
+const app = initializeApp(config);
+
+export const db = getFirestore(app);
